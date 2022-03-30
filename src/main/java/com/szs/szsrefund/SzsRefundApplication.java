6090a5a18c285e1d212a416c4260ac54ddfc384c
dhaud <dhaudgkr@gmail.com>
2022-03-30 19:57:55 +0900

    git init

---
+++ src/main/java/com/szs/szsrefund/SzsRefundApplication.java
@@ -0,0 +1,13 @@
+package com.szs.szsrefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsRefundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzsRefundApplication.class, args);
+    }
+
+}
