27c7c1208b7f0eb0c576abc2387bf7f4d6156ec4
dhaud <dhaudgkr@gmail.com>
2022-06-02 00:10:43 +0900

    Fix: 통신 실패할 경우 3번까지 재시도 하도록 수정

--- src/main/java/com/szs/szsrefund/domain/scrap/service/ScrapService.java
+++ src/main/java/com/szs/szsrefund/domain/scrap/service/ScrapService.java
@@ -71,6 +71,7 @@ public class ScrapService {
         delRefundData(userId);
 
         callScrapApi(requestDto)
+                .retry(3)
                 .subscribe(
                         response -> {
                             try {
